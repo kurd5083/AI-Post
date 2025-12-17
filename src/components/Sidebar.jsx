592efa075e3bc82fcdac01d5e3565b7b6e786e9a
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-17 15:06:58 +0300

    test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.async = true;
-    script.setAttribute("data-telegram-login", "aaaaaaaaaaaaaaaaaaa1_bot"); 
+    script.setAttribute("data-telegram-login", "BEEPOSTING_BOT"); 
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
