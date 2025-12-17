3ba2d0f5fa2a071db4c358a8e3757c2299ce4830
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-17 18:22:03 +0300

    test

--- src/api/createChannelFolder.js
+++ src/api/createChannelFolder.js
@@ -1,6 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const createChannelFolder = async (folderData) => {
+    console.log(folderData)
   const response = await apiClient.post("/channels/folders", folderData);
   return response.data;
 };
