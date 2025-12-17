6553a9dca20477a58f216677db98c74b1a995e64
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-17 17:48:36 +0300

    test

--- src/lib/useUserBalance.jsx
+++ src/lib/useUserBalance.jsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { getUserBalance } from '@/api/getUserBalance';
 
 export const useUserBalance = () => {
     const { data: balance } = useQuery({
         queryKey: ["userBalance"],
-        queryFn: fetchUserBalance,
+        queryFn: getUserBalance,
         enabled: !!localStorage.getItem('accessToken'),
     });
     return { balance };
