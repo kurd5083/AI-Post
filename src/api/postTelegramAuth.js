153090c4eeaf1bb4d3ac9aebae742ce4728fdcc6
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-17 17:10:59 +0300

    test

--- src/api/postTelegramAuth.js
+++ src/api/postTelegramAuth.js
@@ -1,7 +1,6 @@
 import apiClient from './apiClient';
 
 export const postTelegramAuth = async (data) => {
-  console.log('aaaaaa', data)
-  // const response = await apiClient.post('/users/auth/telegram', data)
-  // return response.data; 
+  const response = await apiClient.post('/users/auth/telegram', data)
+  return response.data; 
 };
