5b2486bfdcabcebd81bded4d39741ea18512974e
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-18 16:25:52 +0300

    test

--- src/api/getAutoApprovalStatus.js
+++ src/api/getAutoApprovalStatus.js
@@ -1,12 +1,12 @@
 import apiClient from './apiClient';
 
 export const getAutoApprovalStatus = async (channelId) => {
-  console.log(channelId)
+  console.log(channelId, '123aaaaaaaaa123123')
   const response = await apiClient.get(`/channels/auto-approval/status/${channelId}`);
   return response.data;
 };
 export const updateAutoApprovalStatus = async ({ channelId }) => {
-  console.log(channelId)
+  console.log(channelId, '123123123')
   const response = await apiClient.put(`/channels/auto-approval/status/${channelId}`);
   return response.data;
 };
