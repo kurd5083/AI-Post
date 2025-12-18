05fb80097a2b60f24916bf3c925dd9f1a0c21316
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-18 09:36:54 +0300

    test

--- src/api/getUserById.js
+++ src/api/getUserById.js
@@ -3,6 +3,7 @@ import apiClient from './apiClient';
 export const getUserById = async (id) => {
   try {
     const response = await apiClient.get(`/users/${id}`);
+    console.log(response)
     return response.data; 
   } catch (error) {
     console.error('Ошибка при получении пользователя:', error.message);
--- src/api/postTelegramAuth.js
+++ src/api/postTelegramAuth.js
@@ -1,6 +1,7 @@
 import apiClient from './apiClient';
 
 export const postTelegramAuth = async (data) => {
+  console.log(data)
   const response = await apiClient.post('/users/auth/telegram', data)
   return response.data; 
 };
