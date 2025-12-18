14c57c60fb4b8b46fdf7e478d44cd10bdd84c8b6
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-18 17:54:37 +0300

    test

--- src/lib/useUpdateWorkMode.jsx
+++ src/lib/useUpdateWorkMode.jsx
@@ -7,7 +7,7 @@ export const useUpdateWorkMode = (channelId) => {
   return useMutation({
     mutationFn: ({ workMode, premoderationMinutes }) =>updateWorkMode({ channelId, workMode, premoderationMinutes }),
     onSuccess: (data) => {
-        console.log(":", data);
+      console.log("Результат мутации:", data);
       queryClient.invalidateQueries(["channel", channelId]);
     },
   });
