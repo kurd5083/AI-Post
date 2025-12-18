b93504142204f2dcbdcdc042c715cebca11a1eb4
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-18 11:03:47 +0300

    test

--- src/components/Home/TableGroupsHead.jsx
+++ src/components/Home/TableGroupsHead.jsx
@@ -37,9 +37,9 @@ const TableGroups = () => {
             {/* <mark>{channels.totalChannelsWithoutFolder}</mark> */}
           </TableGroupsHeadBtn>
 
-          {channels.folders?.map((folder) => (
+          {/* {channels.folders?.map((folder) => (
             <TableGroupsHeadBtn><img src={dir} alt="dir icon" /><p>{folder.name}</p> <mark>{folder.channels.length}</mark></TableGroupsHeadBtn>
-          ))}
+          ))} */}
 				</TableGroupsHeadLeft>
 				<TableGroupsHeadRight>
 					<TableGroupsHeadAdd  onClick={() => openPopup("create_channel", "popup_window")}>{isSmall ? "+ Добавить" : "+ Добавить канал"}</TableGroupsHeadAdd>
