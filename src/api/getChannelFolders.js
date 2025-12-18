1c1ec6a938f3fa20257170f6a76cfe3fc836dbcc
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-18 10:37:14 +0300

    test

--- src/api/getChannelFolders.js
+++ src/api/getChannelFolders.js
@@ -1,6 +1,8 @@
 import apiClient from "./apiClient";
 
-export const getChannelFolders = async () => {
-  const response = await apiClient.get(`/channels/folders/empty`);
+export const getChannelFolders = async (ownerTelegramId) => {
+  const response = await apiClient.get(`/channels/folders/empty`, {
+    params: { ownerTelegramId: ownerTelegramId },
+  });
   return response.data;
 };
