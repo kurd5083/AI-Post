b651c74475fbf6b35a3a2156604fc3b64cab58c7
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-18 17:39:44 +0300

    test

--- src/api/channels/updateWorkMode.js
+++ src/api/channels/updateWorkMode.js
@@ -1,6 +1,8 @@
 import apiClient from "@/api/apiClient";
 
-export const updateWorkMode = async ({ channelId }) => {
-  const response = await apiClient.patch(`/channels/${channelId}/work-mode`);
+export const updateWorkMode = async ({ channelId, workMode }) => {
+  const body = { workMode };
+
+  const response = await apiClient.patch(`/channels/${channelId}/work-mode`, workMode);
   return response.data;
 };
