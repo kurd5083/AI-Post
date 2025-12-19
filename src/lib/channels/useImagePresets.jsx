de2fdd7cf3f7fff92b04b952414254bf1ae93e5b
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-19 09:52:41 +0300

    test

--- src/lib/channels/useImagePresets.jsx
+++ src/lib/channels/useImagePresets.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getImagePresets } from "@/api/imagePresets";
+import { getImagePresets } from "@/api/channels/imagePresets";
 
 export const useImagePresets = () => {
   const { data: imagePresets } = useQuery({
