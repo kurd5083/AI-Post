c7e6af330bab9fc78c8d764f42e59f1c44dc4ee7
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-19 11:29:36 +0300

    test

--- src/api/channels/getChannelInterval.js
+++ src/api/channels/getChannelInterval.js
@@ -1,6 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const getChannelInterval = async (channelId) => {
+    console.log(channelId)
   const response = await apiClient.get(`/channel-interval/${channelId}`);
   return response.data;
 };
