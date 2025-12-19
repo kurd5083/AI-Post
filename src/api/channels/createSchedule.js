4e3d6b045604aeef1e1b7ee67d144d720cbb24af
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-19 14:27:03 +0300

    test

--- src/api/channels/createSchedule.js
+++ src/api/channels/createSchedule.js
@@ -1,6 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const createSchedule = async (data) => {
+    console.log(data)
   const response = await apiClient.get(`/channelschedule`, data);
   return response.data;
 };
