3f3d72119cf9e8c12ff1ce0e0c6d1adab3f98a86
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-21 17:49:58 +0300

    test

--- src/api/channels/toggleChannelPosting.js
+++ src/api/channels/toggleChannelPosting.js
@@ -1,6 +1,6 @@
 import apiClient from "@/api/apiClient";
 
 export const toggleChannelPosting = async (channelId) => {
-  const response = await apiClient.post(`/channels/21/posting/toggle`);
+  const response = await apiClient.post(`/channels/21/posting/toggle`, true);
   return response.data;
 };
