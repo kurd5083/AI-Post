d2c88cf6d0d4054fefdbfedefc42d910b78532b8
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-21 17:42:47 +0300

    test

--- src/api/channels/enableChannelPromotion.js
+++ src/api/channels/enableChannelPromotion.js
@@ -1,13 +1,13 @@
 import apiClient from '../apiClient';
 
 export const enableChannelPromotion = async (channelId) => {
-  const response = await apiClient.post(`/channels/${channelId}/promotion/enable`);
+  const response = await apiClient.patch(`/channels/${channelId}/promotion/enable`);
 
   return response.data;
 };
 
 export const disenableChannelPromotion = async (channelId) => {
-  const response = await apiClient.post(`/channels/${channelId}/promotion/disable`);
+  const response = await apiClient.patch(`/channels/${channelId}/promotion/disable`);
 
   return response.data;
 };
--- src/api/channels/toggleChannelPosting.js
+++ src/api/channels/toggleChannelPosting.js
@@ -1,8 +1,6 @@
 import apiClient from "@/api/apiClient";
 
 export const toggleChannelPosting = async (channelId) => {
-  const response = await apiClient.post(
-    `/api/v1/channels/${channelId}/posting/toggle`
-  );
+  const response = await apiClient.post(`/channels/${channelId}/posting/toggle`);
   return response.data;
 };
