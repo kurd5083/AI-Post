07082f1b7a323e478b226150a096d59192198118
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-22 12:04:33 +0300

    test

--- src/lib/channels/filtering/useAddChannelStopword.jsx
+++ src/lib/channels/filtering/useAddChannelStopword.jsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { addChannelStopWord } from "@/api/channels/filtering/addChannelStopWord";
+import { addChannelStopWord } from "@/api/channels/filtering/addChannelStopword";
 
 export const useAddChannelStopWord = () => {
   const queryClient = useQueryClient();
