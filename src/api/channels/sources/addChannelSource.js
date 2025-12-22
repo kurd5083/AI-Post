433284af9c9956e04d978e6fc30275e3ba75151a
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-22 13:52:32 +0300

    test

--- src/api/channels/sources/addChannelSource.js
+++ src/api/channels/sources/addChannelSource.js
@@ -1,7 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const addChannelSource = async (channelId, url) => {
-  const response = await apiClient.post(`/channel-sources/channel/21/add`, {
+  const response = await apiClient.post(`/channel-sources/channel/${21}/add`, {
   "url": "https://t.me/durov"
 });
   return response.data;
