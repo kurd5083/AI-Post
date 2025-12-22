265724b5d4b3b39d820ce39257a272b79b1ed39d
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-22 11:51:43 +0300

    test

--- src/api/channels/filtering/removeChannelStopword.js
+++ src/api/channels/filtering/removeChannelStopword.js
@@ -3,4 +3,4 @@ import apiClient from "@/api/apiClient";
 export const removeChannelStopword = async (channelId, stopword) => {
   const response = await apiClient.delete(`/channels/${channelId}/stopwords/${stopword}`);
   return response.data;
-};
+};  
