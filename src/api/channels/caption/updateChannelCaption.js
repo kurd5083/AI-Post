63c4d78470a651eec24b01b47b53da56f6af0f80
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-22 18:21:03 +0300

    test

--- src/api/channels/caption/updateChannelCaption.js
+++ src/api/channels/caption/updateChannelCaption.js
@@ -1,6 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const updateChannelCaption = async (channelId, value) => {
+  console.log(channelId, value)
   const response = await apiClient.patch(`/channels/${channelId}/caption`, value);
   return response.data;
 };
