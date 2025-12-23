91607723ccff2226b901f899ca457634523787ff
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-23 15:00:04 +0300

    test

--- src/api/authTelegram.js
+++ src/api/authTelegram.js
@@ -1,7 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const authTelegram = async (data) => {
-  console.log(data)
+  console.log(data, 'asghsss')
   const response = await apiClient.post('/users/auth/telegram', data)
   return response.data; 
 };
