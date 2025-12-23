5a0ecf5c4c9422d883e9094f044ecfd9f5a7f4a1
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-23 15:08:19 +0300

    test

--- src/lib/useTelegramBotLink.jsx
+++ src/lib/useTelegramBotLink.jsx
@@ -1,11 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getTelegramBotLink } from '@/api/getTelegramBotLink';
 
-
 export const useTelegramBotLink = () => {
   const { data: botLinkData } = useQuery({
     queryKey: ['telegramBotLink'],
-    queryFn: getTelegramBotLink,
+    queryFn: ()=> getTelegramBotLink(),
   });
 
   return { botLinkData };
