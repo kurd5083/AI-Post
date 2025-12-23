bc3c921915012d297de1d841b9efce75d6be774a
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-23 20:21:44 +0300

    test

--- src/api/channels/getUserChannels.js
+++ src/api/channels/getUserChannels.js
@@ -2,5 +2,6 @@ import apiClient from "@/api/apiClient";
 
 export const getUserChannels = async () => {
   const response = await apiClient.get("/channels/user");
+  console.log(response)
   return response.data;
 };
