53bcaf7a05dd22e4b0522ea3785e06c832f07e2a
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-24 14:09:05 +0300

    test

--- src/api/channels/image-generation/updateChannelImagePreset.js
+++ src/api/channels/image-generation/updateChannelImagePreset.js
@@ -2,6 +2,6 @@ import apiClient from "@/api/apiClient";
 
 export const updateChannelImagePreset = async (channelId, presetId) => {
     console.log(channelId, presetId)
-  const response = await apiClient.put(`/image-generation/channels/${channelId}/preset`, presetId);
+  const response = await apiClient.put(`/image-generation/channels/${channelId}/preset`, { presetId });
   return response.data;
 };
