1aed1e07ef4c88b031e4f0b27f22edab07289499
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-24 14:01:44 +0300

    test

--- src/lib/channels/image-generation/useGetChannelImagePreset.jsx
+++ src/lib/channels/image-generation/useGetChannelImagePreset.jsx
@@ -2,11 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { getChannelImagePreset } from '@/api/channels/image-generation/getChannelImagePreset';
 
 export const useGetChannelImagePreset = (channelId) => {
-  const { data: imageChannelPresets } = useQuery({
+  const { data: imageChannelPreset } = useQuery({
     queryKey: ['channel-image-preset', channelId],
     queryFn: () => getChannelImagePreset(channelId),
     enabled: !!channelId,
   });
 
-  return { imageChannelPresets };
+  return { imageChannelPreset };
 };
