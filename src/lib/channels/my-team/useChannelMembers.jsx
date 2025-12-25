9deb275d08541d7c12fa4805591ab190c906ba35
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-25 13:51:30 +0300

    test

--- src/lib/channels/my-team/useChannelMembers.jsx
+++ src/lib/channels/my-team/useChannelMembers.jsx
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getChannelMembers } from "@/api/channels/my-team/getChannelMembers";
 
 export const useChannelMembers = (channelName) => {
-  const { data: members } = useQuery({
+  const { data: members, isLoading: membersLoading } = useQuery({
     queryKey: ["channel-members", channelName],
     queryFn: () => getChannelMembers(channelName),
     enabled: !!channelName,
   });
 
-  return { members };
+  return { members, membersLoading };
 };
