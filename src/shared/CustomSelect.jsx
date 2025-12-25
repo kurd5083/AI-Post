9585a6bca605bb9ea5068906b6ebacd2f5c0ab72
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-25 14:47:00 +0300

    test

--- src/shared/CustomSelect.jsx
+++ src/shared/CustomSelect.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import arrow from "@/assets/arrow.svg";
 
-const CustomSelect = ({ options, value, onChange, placeholder = "Выберите значение", padding, border=true }) => {
+const CustomSelect = ({ options, value, onChange, placeholder = "Выберите значение", padding=true, border=true }) => {
   const [open, setOpen] = useState(false);
   const selectRef = useRef(null);
 
