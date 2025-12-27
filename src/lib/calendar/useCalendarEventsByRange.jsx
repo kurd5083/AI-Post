8d206dde3a112b8663cfb913c4bad1c0ba89fc4f
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-27 15:15:44 +0300

    test

--- src/lib/calendar/useCalendarEventsByRange.jsx
+++ src/lib/calendar/useCalendarEventsByRange.jsx
@@ -2,11 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { getCalendarEventsByRange } from "@/api/calendar/getCalendarEventsByRange";
 
 export const useCalendarEventsByRange = ({ channelId, startDate, endDate }) => {
-    const { data: events } = useQuery({
-        queryKey: ["calendar-events", channelId],
-        queryFn: () => getCalendarEventsByRange({ channelId, startDate, endDate }),
-        enabled: !!channelId,
-    });
+  const { data: events = [] } = useQuery({
+    queryKey: ["calendar-events", channelId, startDate, endDate],
+    queryFn: () => getCalendarEventsByRange({ channelId, startDate, endDate }),
+    enabled: !!channelId && !!startDate && !!endDate,
+  });
 
-    return { events };
+  return { events };
 };
+
