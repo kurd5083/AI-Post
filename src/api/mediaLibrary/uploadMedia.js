a6ebe818c18dc1397d3a310f8d358bf6d18d5263
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-28 13:54:49 +0300

    test

--- src/api/mediaLibrary/uploadMedia.js
+++ src/api/mediaLibrary/uploadMedia.js
@@ -1,21 +1,19 @@
 import apiClient from "@/api/apiClient";
 
-const fileToBase64 = (file) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (err) => reject(err);
-  });
-
 export const uploadMediaLibrary = async (files) => {
-  const base64Files = await Promise.all(files.map(file => fileToBase64(file)));
-    console.log(base64Files)
+    console.log(files)
+  if (!files || files.length === 0) {
+    throw new Error("Необходимо выбрать хотя бы один файл");
+  }
 
-    const response = await apiClient.post("/media-library/upload", {params: base64Files});
-//   const response = await apiClient.post("/media-library/upload", {
-//     files: base64Files
-//   });
+  const formData = new FormData();
+  files.forEach((file) => formData.append("files", file));
+
+  const response = await apiClient.post("/media-library/upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
   return response.data;
 };
