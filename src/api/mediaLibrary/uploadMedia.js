5538932a8b12a4da46d9490ff8005da099d62ac3
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-28 13:18:51 +0300

    test

--- src/api/mediaLibrary/uploadMedia.js
+++ src/api/mediaLibrary/uploadMedia.js
@@ -4,13 +4,17 @@ const fileToBase64 = (file) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
+    reader.onload = () => {
+      // убираем префикс data:image/...;base64,
+      const base64String = reader.result.split(',')[1];
+      resolve(base64String);
+    };
     reader.onerror = (err) => reject(err);
   });
 
 export const uploadMediaLibrary = async (files) => {
   const base64Files = await Promise.all(files.map(file => fileToBase64(file)));
-    console.log(base64Files)
+
   const response = await apiClient.post("/media-library/upload", {
     files: base64Files
   });
