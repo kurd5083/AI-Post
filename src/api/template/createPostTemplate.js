0338ea6e6291b3196624516b7e055960d1437e99
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-28 14:05:48 +0300

    test

--- src/api/template/createPostTemplate.js
+++ src/api/template/createPostTemplate.js
@@ -2,6 +2,13 @@ import apiClient from "@/api/apiClient";
 
 export const createPostTemplate = async (templateData) => {
     console.log(templateData)
-  const response = await apiClient.post("/post-templates", templateData);
+  const response = await apiClient.post("/post-templates", {
+  "category": "MARKETING",
+  "title": "Объявление о продукте",
+  "content": "🚀 Представляем новый продукт!\n\n✨ Основные преимущества:\n\n- Быстро\n- Удобно\n- Надёжно",
+  "hashtags": ["#продукт", "#анонс", "#маркетинг"],
+  "rating": 4.8,
+  "isActive": true
+});
   return response.data;
 };
