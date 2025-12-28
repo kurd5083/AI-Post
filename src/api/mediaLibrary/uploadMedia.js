47313e3bdb10eafb87bf1d44d62f3d9bb9c0955f
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-28 13:49:04 +0300

    test

--- src/api/mediaLibrary/uploadMedia.js
+++ src/api/mediaLibrary/uploadMedia.js
@@ -11,12 +11,8 @@ const fileToBase64 = (file) =>
 export const uploadMediaLibrary = async (files) => {
   const base64Files = await Promise.all(files.map(file => fileToBase64(file)));
     console.log(base64Files)
-   const testFiles = [
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA...", // реальная Base64 строка
-  "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD..."   // и так далее
-];
 
-    const response = await apiClient.post("/media-library/upload", testFiles);
+    const response = await apiClient.post("/media-library/upload", {params: base64Files});
 //   const response = await apiClient.post("/media-library/upload", {
 //     files: base64Files
 //   });
