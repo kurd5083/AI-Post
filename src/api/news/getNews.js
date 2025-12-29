52628c0511129aabf28bb05a4362258be4775287
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-29 13:39:08 +0300

    test

--- src/api/news/getNews.js
+++ src/api/news/getNews.js
@@ -1,6 +1,7 @@
 import apiClient from "@/api/apiClient";
 
 export const getNews = async (filters) => {
+  console.log(filters)
   const response = await apiClient.get("/news", {filters});
 
   return response.data;
