7a9ae93ed22bc84d8f5cf2589481c27ac0b25462
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-30 15:28:47 +0300

    test

--- src/api/tgStat/getMentions.js
+++ src/api/tgStat/getMentions.js
@@ -1,9 +1,12 @@
 import apiClient from "@/api/apiClient";
 
 export const getMentions = async (channelIds) => {
-  console.log(channelIds, 'zzzzzzzz')
-  const response = await apiClient.get(`/tg-stat/mentions`, { params: {
-      channelIds: channelIds.join(","),
-    },});
+  console.log(channelIds.join(","), 'zzzzzzzz')
+  console.log(localStorage.getItem('accessToken'));
+  const response = await apiClient.get(`/tg-stat/mentions`, {
+    params: {
+      channelIds: "42,33,29,28,27,24,23"
+    },
+  });
   return response.data;
 };
