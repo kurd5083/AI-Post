5fa18e4029a2bf21fae84573e9170d59e9443a07
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2025-12-30 11:40:26 +0300

    test

--- src/api/calendar/getCalendarEventsByRange.js
+++ src/api/calendar/getCalendarEventsByRange.js
@@ -1,6 +1,6 @@
 import apiClient from "@/api/apiClient";
 
 export const getCalendarEventsByRange = async (startDate, endDate) => {
-  const response = await apiClient.get(`/calendar/channel/range`, { startDate, endDate });
+  const response = await apiClient.get(`/calendar/range`, { startDate, endDate });
   return response.data;
 };
