73911f735294b24691ed383ece7877f91fc0c83f
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2026-01-02 07:29:08 +0300

    test

--- src/components/CardPablish.jsx
+++ src/components/CardPablish.jsx
@@ -160,6 +160,8 @@ const ImageItemWrapper = styled.div`
 `;
 const ImageItem = styled.img`
   object-fit: cover;
+  width: 100%;
+  height: 100%;
 `
 const Overlay = styled.div`
   position: absolute;
