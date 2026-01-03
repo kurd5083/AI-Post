faa5b3e68354a16ec8c59b8daa9a4bf5e9c8bd92
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2026-01-03 11:36:22 +0300

    test

--- src/components/PopupWindow/CreateCalendarEventPopup.jsx
+++ src/components/PopupWindow/CreateCalendarEventPopup.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import CloseIcon from "@/icons/CloseIcon";
 import CustomSelectSec from "@/shared/CustomSelectSec";
