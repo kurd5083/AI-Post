d087b8d1ccdbb9ea7d8b74c2e1e1d084fe3cb7a2
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2026-01-04 17:28:31 +0300

    test

--- src/lib/news/useCopyNewsToChannel.jsx
+++ src/lib/news/useCopyNewsToChannel.jsx
@@ -2,6 +2,8 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { copyNewsToChannel } from "@/api/news/copyNewsToChannel";
 
 export const useCopyNewsToChannel = () => {
+  const queryClient = useQueryClient();
+  
   return useMutation({
     mutationFn: ({ id, data }) => copyNewsToChannel(id, data),
     onSuccess: (id) => {
