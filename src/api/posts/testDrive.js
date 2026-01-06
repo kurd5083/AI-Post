495eeb4b0a773e28bd780d3a55790449191105df
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2026-01-06 10:02:53 +0300

    test

--- src/api/posts/testDrive.js
+++ src/api/posts/testDrive.js
@@ -3,4 +3,4 @@ import apiClient from "@/api/apiClient";
 export const testDrivePrompt = async ({ promtManage, channelId }) => {
   const response = await apiClient.post("/prompt/test-drive", { promtManage, channelId });
   return response.data;
-};
+}; 
