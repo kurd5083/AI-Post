d3fdd820112fc41b6e45ed897fb205b0b1921b3e
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2026-01-06 09:52:25 +0300

    test

--- src/api/tgStat/getChannelStat.js
+++ src/api/tgStat/getChannelStat.js
@@ -3,7 +3,6 @@ import apiClient from "@/api/apiClient";
 export const getChannelStat = async (channelIds) => {
   const response = await apiClient.get("/tg-stat/channel-stat", {params: {
     channelIds: channelIds.join(","),
-    channelId: 44
   }});
   return response.data;
 };
--- src/api/tgStat/getMentions.js
+++ src/api/tgStat/getMentions.js
@@ -3,7 +3,6 @@ import apiClient from "@/api/apiClient";
 export const getMentions = async (channelIds) => {
   const response = await apiClient.get(`/tg-stat/mentions`, {params: {
     channelIds: channelIds.join(","),
-    channelId: 44
   }});
   return response.data;
 };
