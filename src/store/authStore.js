68771f0db5854794c92a9e4d693000bf7dd9763e
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2026-01-10 18:04:36 +0300

    test

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,30 +1,36 @@
 import { create } from 'zustand';
 
 export const useAuthStore = create((set, get) => {
-  const token = localStorage.getItem("accessToken");
-  const refreshToken = localStorage.getItem("refreshToken");
-  const user = JSON.parse(localStorage.getItem("user") || "null");
+    const token = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+    const user = JSON.parse(localStorage.getItem("user") || "null");
 
-  return {
-    token,
-    refreshToken,
-    user,
-    get isAuthenticated() {
-      return !!get().token;
-    },
+    return {
+        token,
+        refreshToken,
+        user,
+        get isAuthenticated() {
+            return !!get().token;
+        },
 
-    login: (t, r, u) => {
-      localStorage.setItem("accessToken", t);
-      localStorage.setItem("refreshToken", r);
-      if(u) localStorage.setItem("user", JSON.stringify(u));
-      set({ token: t, refreshToken: r, user: u });
-    },
+        login: (t, r, u) => {
+            localStorage.setItem("accessToken", t);
+            localStorage.setItem("refreshToken", r);
+            if (u) localStorage.setItem("user", JSON.stringify(u));
+            set({ token: t, refreshToken: r, user: u });
+        },
 
-    logout: () => {
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-      localStorage.removeItem("user");
-      set({ token: null, refreshToken: null, user: null });
-    },
-  };
+        logout: () => {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            localStorage.removeItem("user");
+
+            set({ token: null, refreshToken: null, user: null });
+
+            const popupStore = usePopupStore.getState();
+            if (popupStore.popup) {
+                popupStore.changeContent(null);
+            }
+        }
+    };
 });
